9c07882153223b6b7dde0e326d77fc16060f25f1
Wolfqng <tkelly6397@gmail.com>
2023-05-23 20:13:16 -0400

    Colors and ui

--- flowsheet.js
+++ flowsheet.js
@@ -1,8 +1,6 @@
-const green = "#CEF6CE";
-const red = "#F6CECE";
+const colors = ["#CEF6CE", "#F6CECE", "#44A1A0", "#FF9F1C"];
 
-var customColors = [];
-var color = green;
+var color = undefined;
 
 // Selection
 var genboxes = document.getElementsByClassName('genbox');
@@ -10,8 +8,47 @@ var genboxes = document.getElementsByClassName('genbox');
 for(let i = 0; i < genboxes.length; i++) {
     let box = genboxes[i];
     box.addEventListener("click", function() {
-        box.style.backgroundColor = color;
+
+        if(color != undefined) {
+            box.style.backgroundColor = color;
+        }
     });
+}
+
+// Inject DOM elements
+injectDom();
+
+function injectDom() {
+    let container = document.createElement("DIV");
+    container.id = "color_my_flowsheet";
+
+    let noColor = createColorButton(undefined);
+    container.appendChild(noColor);
+
+    for(let i = 0; i < colors.length; i++) {
+        let newColor = colors[i];
+        let btn = createColorButton(newColor);
+        container.appendChild(btn);
+    }
+
+    document.body.appendChild(container);
+}
+
+function createColorButton(newColor) {
+    let btn = document.createElement("button");
+    btn.classList.add("flowsheet_color_btn");
+
+    btn.style.backgroundColor = newColor;
+
+    if(newColor == undefined) {
+        btn.innerText = "X";
+    }
+
+    btn.addEventListener("click", function() {
+        color = newColor;
+    });
+
+    return btn;
 }
 
 // Let the console know the extension is improted
